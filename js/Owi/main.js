bef41ab15622157617dd31cbccf3756735c8b250
marcio lira <machollanda@gmail.com>
2021-01-03 15:28:33 -0300

    commit img roll

---
+++ js/Owi/main.js
@@ -0,0 +1,34 @@
+$('.owl-carousel').owlCarousel({
+    center: true,
+    items:2,
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+$('.nonloop').owlCarousel({
+    center: true,
+    items:2,
+    loop:false,
+    margin:10,
+    responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:5
+        }
+    }
+});
